f052f3de7cedd4f51049298a3ace988faead3d10
marisel <140407389+betingrich@users.noreply.github.com>
2024-07-21 20:56:22 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-    default: Maher_Zubair,
+    default: Marisel_King,
     useMultiFileAuthState,
     delay,
     makeCacheableSignalKeyStore,
     Browsers
-} = require("maher-zubair-baileys");
+} = require("marisel-King");
 
 function removeFile(FilePath){
     if(!fs.existsSync(FilePath)) return false;
@@ -20,13 +20,13 @@ function removeFile(FilePath){
 router.get('/', async (req, res) => {
     const id = makeid();
     let num = req.query.number;
-        async function SIGMA_MD_PAIR_CODE() {
+        async function Marisel_King_PAIR_CODE() {
         const {
             state,
             saveCreds
         } = await useMultiFileAuthState('./temp/'+id)
      try {
-            let Pair_Code_By_Maher_Zubair = Maher_Zubair({
+            let Pair_Code_By_Marisel_King = Marisel_King({
                 auth: {
                     creds: state.creds,
                     keys: makeCacheableSignalKeyStore(state.keys, pino({level: "fatal"}).child({level: "fatal"})),
@@ -35,16 +35,16 @@ router.get('/', async (req, res) => {
                 logger: pino({level: "fatal"}).child({level: "fatal"}),
                 browser: ["Chrome (Linux)", "", ""]
              });
-             if(!Pair_Code_By_Maher_Zubair.authState.creds.registered) {
+             if(!Pair_Code_By_Marisel_King.authState.creds.registered) {
                 await delay(1500);
                         num = num.replace(/[^0-9]/g,'');
-                            const code = await Pair_Code_By_Maher_Zubair.requestPairingCode(num)
+                            const code = await Pair_Code_By_Marisel_King.requestPairingCode(num)
                  if(!res.headersSent){
                  await res.send({code});
                      }
                  }
-            Pair_Code_By_Maher_Zubair.ev.on('creds.update', saveCreds)
-            Pair_Code_By_Maher_Zubair.ev.on("connection.update", async (s) => {
+            Pair_Code_By_Marisel_King.ev.on('creds.update', saveCreds)
+            Pair_Code_By_Marisel_King.ev.on("connection.update", async (s) => {
                 const {
                     connection,
                     lastDisconnect
@@ -54,39 +54,39 @@ router.get('/', async (req, res) => {
                 let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
                 await delay(800);
                let b64data = Buffer.from(data).toString('base64');
-               let session = await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id, { text: '' + b64data });
+               let session = await Pair_Code_By_Marisel_King.sendMessage(Pair_Code_By_Marisel_King.user.id, { text: '' + b64data });
 
-               let SIGMA_MD_TEXT = `
-*BOTGURU 𝗠𝗗 𝗖𝗢𝗡𝗡𝗘𝗖𝗧𝗘𝗗*
-*BOTGURU*
-*𝗟𝗘𝗚𝗜𝗧_𝗕𝗢𝗧*
+               let Marisel_King_TEXT = `
+*𝗞𝗜𝗡𝗚 𝗠𝗔𝗥𝗜𝗦𝗘𝗟 𝗖𝗢𝗡𝗡𝗘𝗖𝗧𝗘𝗗*
+*𝑴𝒂𝒓𝒊𝒔𝒆𝒍*
+*𝑨𝒎𝒐𝒏𝒈 𝑻𝒉𝒆 𝑪𝒉𝒂𝒎𝒑𝒊𝒐𝒏 𝑩𝒐𝒕𝒔*
 ____________________________________
 ╔════◇
 ║『 𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥』
 
-║ ❒ *Mr Guru*  : _https://t.me/Guru_1st_
+║ ❒ *M𝒂𝒓𝒊𝒔𝒆𝒍*  : _254740007567_
 
 ╚════════════════════❒
 ╔═════◇
 ║ 『••• 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 •••』
 
-║ ❒ *Telegram* : _https://t.me/botgurumd_
+║ ❒ *𝑮𝒊𝒕𝒉𝒖𝒃* : _https://github.com/betingrich_
 
 ║ 
 ╚════════════════════╝ 
- *BOTGURU*
+ *𝑴𝒂𝒓𝒊𝒔𝒆𝒍*
 ___________________________________
 
-Don't Forget To Give Star🌟 To My Repo`
- await Pair_Code_By_Maher_Zubair.sendMessage(Pair_Code_By_Maher_Zubair.user.id,{text:SIGMA_MD_TEXT},{quoted:session})
+𝑫𝒐𝒏𝒕 𝑭𝒐𝒓𝒈𝒆𝒕 𝒕𝒐☆ 𝑻𝒉𝒆 𝒓𝒆𝒑𝒐`
+ await Pair_Code_By_Marisel_King.sendMessage(Pair_Code_By_Marisel_King.user.id,{text:Marisel_King_TEXT},{quoted:session})
  
 
         await delay(100);
-        await Pair_Code_By_Maher_Zubair.ws.close();
+        await Pair_Code_By_Marisel_King.ws.close();
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    SIGMA_MD_PAIR_CODE();
+                    Marisel_King_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -97,6 +97,6 @@ Don't Forget To Give Star🌟 To My Repo`
          }
         }
     }
-    return await SIGMA_MD_PAIR_CODE()
+    return await Marisel_King_PAIR_CODE()
 });
 module.exports = router
